6878a4fd00a6cdc350168a5fa7024bdeb02ec2f4
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-06-07 18:53:30 +0100

    (Client) refactor screen

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,19 +1,30 @@
 import TextArea from "./components/TextArea";
 import Header from "./components/Header";
+import AreaText from "./components/AreaText";
+
 export default function App() {
+  fetch("http://localhost:3000/",{
+    method:'GET',
+    headers:{
+      'Content-Type':'application/json',
+    }
+  })
+  .then((resp)=>resp.json())
+  .then((data)=>{
+     
+  })
+  .catch((err)=>console.log(err));
   return (
     <>
+  
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    
+     
    <Header/>
+   
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <AreaText resp="Hello world"/>
      <TextArea/>
-      <p className="mt-10 text-center text-sm text-gray-500">
-        Not a member?{' '}
-        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-          Start a 14 day free trial
-        </a>
-      </p>
+    
     </div>
   </div>
     </>
--- packages/client/src/components/TextArea.jsx
+++ packages/client/src/components/TextArea.jsx
@@ -19,11 +19,7 @@ export default function TextArea(){
                   </button>
                   <div class="flex pl-0 space-x-1 sm:pl-2">
                       
-                      <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                      <svg class="h-6 w-6 text-gray"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"/>
-                        </svg>
-                        </button>
+                     
                       <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
                       <svg class="h-6 w-6 text-gray"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
@@ -34,7 +30,11 @@ export default function TextArea(){
               </div>
           </div>
         </form>
-        <p class="ml-auto text-xs text-gray-500 dark:text-gray-400">Remember, contributions to this topic should follow our <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Community Guidelines</a>.</p>
+        <p class="ml-auto text-xs text-gray-500 dark:text-gray-400">      
+           Lembre-se de que as contribuições para este project devem ser feita,
+          <a href="https://github.com/alfredo003/STranslator" class="text-blue-600 dark:text-blue-500 hover:underline">
+             nas Diretrizes do projecto</a>.
+            </p>
         </div>
 
      
